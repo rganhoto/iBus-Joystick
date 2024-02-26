35f48a0d09883c8bc181790c9c02f7d8e82ae759
Rui Ganhoto <ganhoto@live.com>
2024-02-26 21:55:10 +0000

    add more buttons to arm/flight mode

--- iBusJoystick/iBusJoystick.ino
+++ iBusJoystick/iBusJoystick.ino
@@ -2,7 +2,7 @@
 #include "FlySkyIBus.h"
 
 // Joystick setup object
-Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_MULTI_AXIS, 32, 0, true, true, false, false, false, false, true, true, false, false, false);
+Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_JOYSTICK, 32, 0, true, true, false, false, false, false, true, true, false, false, false);
 //                ( HID report ID             Joystick Type,           btn hat  X     Y     Z      Rx     Ry     Rz   Rudd   Thro  Acc   Break  Steer)
 
 void setup()
@@ -26,10 +26,44 @@ void loop()
   int pitch = map(IBus.readChannel(1), 1000, 2000, 0, 1023);
   int throttle = map(IBus.readChannel(2), 1000, 2000, 0, 1023);
   int yaw = map(IBus.readChannel(3), 1000, 2000, 0, 1023);
+  int arm = map(IBus.readChannel(4), 1000, 2000, 0, 1023);
+  int mode = map(IBus.readChannel(5), 1000, 2000, 0, 1023);
+
+
 
   // Map values to the Joystick object
   Joystick.setXAxis(roll);
   Joystick.setYAxis(pitch);
   Joystick.setThrottle(throttle);
   Joystick.setRudder(yaw);
+  if(arm < 512)
+  {
+    Joystick.setButton(0, 1);
+    Joystick.setButton(1, 0);
+  }
+  else
+  {
+    Joystick.setButton(0, 0);
+    Joystick.setButton(1, 1);
+  }
+
+  if(mode < 340)
+  {
+    Joystick.setButton(2, 1);
+    Joystick.setButton(3, 0);
+    Joystick.setButton(4, 0);
+  }
+  else if(mode < 680)
+  {
+    Joystick.setButton(2, 0);
+    Joystick.setButton(3, 1);
+    Joystick.setButton(4, 0);
+  }
+  else
+  {
+    Joystick.setButton(2, 0);
+    Joystick.setButton(3, 0);
+    Joystick.setButton(4, 1);
+  }
+  
 }
